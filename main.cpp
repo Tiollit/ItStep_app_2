433df923ecf1cb5caa4f729c7a10f9d9f56a88cb
Tiollit <Tiollit7@gmail.com>
2022-10-08 20:04:24 +0200

    Lab 6 mod

--- main.cpp
+++ main.cpp
@@ -2,15 +2,18 @@
 using namespace std;
 int main()
 {
-    int a, b, c, d = 0;
+    int a, b, c, d = 0, e = 0;
     for (int i = 100; i <= 999; i++)
     {
         a = i / 100;                   // число сотень
         b = (i - a*100) / 10;          // число десятків
         c = i - a*100 - b*10;          // число одиниць
-        if (a == b || a == c || b == c) d = d + 1;       
-    }
-    cout << "Count of numbers with 2 same symbols is: " << d << endl;
+        if (a == b || a == c || b == c) d = d + 1;       // число цифр де хоча б 2 цифри однакові
+        if (a == b && b == c && a == c) e = e + 1;       // число цифр де всі 3 цифри однакові (їх всього 9 - але можна записати кодом)
+    } 
+
+    cout << "Count of numbers with at least 2 same symbols is: " << d << endl;
+    cout << "Count of numbers with only 2 same symbols is: " << d - e << endl;
     
     return 0;
 }
