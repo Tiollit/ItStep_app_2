db08ef6205106a7bc4d377071192ea393cebf575
Tiollit <Tiollit7@gmail.com>
2022-10-08 21:23:09 +0200

    Lab 8 part 1

--- main.cpp
+++ main.cpp
@@ -2,43 +2,24 @@
 using namespace std;
 int main()
 {
-    //----------------Пред-перевірки--------------
-    /*int size;
-    cout << "=>";
-    cin >> size;
-    int i = size;*/
-    /*while(i < 10)
+    int a, b, d, e, c = 1, n = 1;
+    cout << "Incert random number:" << endl;
+    cin >> a;
+    e = a; d = a;
+    while ((e /= 10) > 0) n++;                 // n - кількість цифр в числі
+    // cout << n;    
+    for (int i = n; i >= 0; i--)
     {
-        cout << "* ";
-        i++;
-    }*/
-    //----------------Пост-перевірки---------------
-    //int age;
-    ////
-    //do
-    //{
-    //  system("cls");
-    //  cout << "Enter your age: ";
-    //  cin >> age;
-    //  /*if (age >= 18)
-    //  {
-    //      cout << "Sucsses!" << endl;
-    //      system("pause");
-    //      break;
-    //  }
-    //  else
-    //  {
-    //      cout << "Error! Incorect age: Your age must be more 18+" << endl;
-    //      system("pause");
-    //  }*/
-    //  
-    //} while (age < 18);
-    //system("cls");
-    //cout << "Exit..." << endl;
-    //----------------Пошаговий---------------
-    for (int i = 0; i < 10; i += 3)
-    {
-        cout << i << "\t";
+        for (int k = n; k >= 0; k--)
+        {
+            c = c * 10;                            
+        }
+        b = a / c;                             // перша цифра числа
+        if (b == 3 || b == 6) d = d - b * c;   // прибираємо цифру якщо вона = 3 або 6 але залишаємо 0
+        a = a - b * c;                         // забираємо першу цифру
+        c = 1;
+        n = n - 1;
     }
-    cout << endl;
+    cout << "New number is: " << d << endl;
     return 0;
+}
