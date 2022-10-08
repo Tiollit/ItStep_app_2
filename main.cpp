916ebf0e114a30db861f65c1cc3b68148bf5c6c9
Tiollit <Tiollit7@gmail.com>
2022-10-08 17:22:33 +0200

    Lab 16 done

--- main.cpp
+++ main.cpp
@@ -2,43 +2,42 @@
 using namespace std;
 int main()
 {
-    //----------------Пред-перевірки--------------
-    /*int size;
-    cout << "=>";
-    cin >> size;
-    int i = size;*/
-    /*while(i < 10)
+    int a, b, m, n;
+    cout << "Incert A B:" << endl;
+    cin >> a >> b;
+    if (a > b)
     {
-        cout << "* ";
-        i++;
-    }*/
-    //----------------Пост-перевірки---------------
-    //int age;
-    ////
-    //do
-    //{
-    //  system("cls");
-    //  cout << "Enter your age: ";
-    //  cin >> age;
-    //  /*if (age >= 18)
-    //  {
-    //      cout << "Sucsses!" << endl;
-    //      system("pause");
-    //      break;
-    //  }
-    //  else
-    //  {
-    //      cout << "Error! Incorect age: Your age must be more 18+" << endl;
-    //      system("pause");
-    //  }*/
-    //  
-    //} while (age < 18);
-    //system("cls");
-    //cout << "Exit..." << endl;
-    //----------------Пошаговий---------------
-    for (int i = 0; i < 10; i += 3)
-    {
-        cout << i << "\t";
+        m = b;
+        n = a;
+        b = n;
+        a = m;     
     }
     cout << endl;
+    cout << "unpaired" << endl;
+    for (int i = a; i <= b; i++)
+    {
+        if (i % 2 != 0)
+        {
+            cout << i << "\t";
+        }        
+    }
+    cout << endl;
+    cout << "paired" << endl;
+    for (int i = a; i <= b; i++)
+    {
+        if (i % 2 == 0)
+        {
+            cout << i << "\t";
+        }
+    }
+    cout << endl;
+    cout << "multiples 7" << endl;
+    for (int i = a; i <= b; i++)
+    {
+        if (i % 7 == 0)
+        {
+            cout << i << "\t";
+        }
+    }          
     return 0;
+}
