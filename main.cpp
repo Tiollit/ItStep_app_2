2bbc362f76d7d5b1a91a6ee0b0e87b63fccdee1b
Tiollit <Tiollit7@gmail.com>
2022-10-08 19:05:23 +0200

    Lab 2 done

--- main.cpp
+++ main.cpp
@@ -2,45 +2,14 @@
 using namespace std;
 int main()
 {
-
-    //----------------Пред-перевірки--------------
-    /*int size;
-    cout << "=>";
-    cin >> size;
-    int i = size;*/
-    /*while(i < 10)
+    int c = 1, d = 1, x, y;
+    cout << "Insert 2 numbers X and Y:" << endl;
+    cin >> x >> y;
+    for (int i = 0; i < y; i++)
     {
-        cout << "* ";
-        i++;
-    }*/
-    //----------------Пост-перевірки---------------
-    //int age;
-    ////
-    //do
-    //{
-    //  system("cls");
-    //  cout << "Enter your age: ";
-    //  cin >> age;
-    //  /*if (age >= 18)
-    //  {
-    //      cout << "Sucsses!" << endl;
-    //      system("pause");
-    //      break;
-    //  }
-    //  else
-    //  {
-    //      cout << "Error! Incorect age: Your age must be more 18+" << endl;
-    //      system("pause");
-    //  }*/
-    //  
-    //} while (age < 18);
-    //system("cls");
-    //cout << "Exit..." << endl;
-    //----------------Пошаговий---------------
-    for (int i = 0; i < 10; i += 3)
-    {
-        cout << i << "\t";
+        c = c * x;
+        d = d * y;
     }
-    cout << endl;
+    cout << "x = " << c << " ; y = " << d << endl;
     return 0;
 }
