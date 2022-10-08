aafd259b20584cebc5c78853627a96cb0717af19
Tiollit <Tiollit7@gmail.com>
2022-10-08 17:30:00 +0200

    Lab 17 done 2

--- main.cpp
+++ main.cpp
@@ -2,9 +2,16 @@
 using namespace std;
 int main()
 {
-    int a, b, c = 0;
+    int a, b, m, n, c = 0;
     cout << "Incert numbers A B:" << endl;
     cin >> a >> b;
+    if (a > b)
+    {
+        m = b;
+        n = a;
+        b = n;
+        a = m;
+    }
     for (int i = a; i < b; i++)
     {
         c = c + i;
