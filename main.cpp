2511ed0510b5e1bfa1bb593f8f0d65c98b8a2ea6
Tiollit <Tiollit7@gmail.com>
2022-10-08 18:03:32 +0200

    Lab 19 done

--- main.cpp
+++ main.cpp
@@ -2,43 +2,32 @@
 using namespace std;
 int main()
 {
-    //----------------Пред-перевірки--------------
-    /*int size;
-    cout << "=>";
-    cin >> size;
-    int i = size;*/
-    /*while(i < 10)
+    int a, b, c = 0;
+    a = rand() % 500;
+    cout << "Incert number from 0 to 500:" << endl;
+    do
     {
-        cout << "* ";
-        i++;
-    }*/
-    //----------------Пост-перевірки---------------
-    //int age;
-    ////
-    //do
-    //{
-    //  system("cls");
-    //  cout << "Enter your age: ";
-    //  cin >> age;
-    //  /*if (age >= 18)
-    //  {
-    //      cout << "Sucsses!" << endl;
-    //      system("pause");
-    //      break;
-    //  }
-    //  else
-    //  {
-    //      cout << "Error! Incorect age: Your age must be more 18+" << endl;
-    //      system("pause");
-    //  }*/
-    //  
-    //} while (age < 18);
-    //system("cls");
-    //cout << "Exit..." << endl;
-    //----------------Пошаговий---------------
-    for (int i = 0; i < 10; i += 3)
-    {
-        cout << i << "\t";
-    }
-    cout << endl;
+        cin >> b;
+        c = c + 1;
+        if (b == 0)
+        {
+            cout << "End Game." << " Number of operations:" << c << endl;
+            break;
+        }
+        else if (a > b)
+        {
+            cout << "Incerted number is less then generated." << " Number of operations:" << c << endl;
+        }
+        else if (a < b)
+        {
+            cout << "Incerted number is more then generated." << " Number of operations:" << c << endl;
+        }
+        else if (a == b)
+        {
+            cout << "You found right number." << " Number of operations:" << c << endl;
+            break;
+        }
+    } while (true);  
+
     return 0;
+}
