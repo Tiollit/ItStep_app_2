b4ab2eaef196053d9b12fa6935bdd04dfa0a82c8
Tiollit <Tiollit7@gmail.com>
2022-10-08 16:38:10 +0200

    Lab 15 done

--- main.cpp
+++ main.cpp
@@ -2,43 +2,13 @@
 using namespace std;
 int main()
 {
-    //----------------Пред-перевірки--------------
-    /*int size;
-    cout << "=>";
-    cin >> size;
-    int i = size;*/
-    /*while(i < 10)
-    {
-        cout << "* ";
-        i++;
-    }*/
-    //----------------Пост-перевірки---------------
-    //int age;
-    ////
-    //do
-    //{
-    //  system("cls");
-    //  cout << "Enter your age: ";
-    //  cin >> age;
-    //  /*if (age >= 18)
-    //  {
-    //      cout << "Sucsses!" << endl;
-    //      system("pause");
-    //      break;
-    //  }
-    //  else
-    //  {
-    //      cout << "Error! Incorect age: Your age must be more 18+" << endl;
-    //      system("pause");
-    //  }*/
-    //  
-    //} while (age < 18);
-    //system("cls");
-    //cout << "Exit..." << endl;
-    //----------------Пошаговий---------------
-    for (int i = 0; i < 10; i += 3)
+    int a;
+    cout << "Incert number:" << endl;
+    cin >> a; 
+    for (int i = 0; i <= a; i++)
     {
         cout << i << "\t";
     }
-    cout << endl;
+
     return 0;
+}
