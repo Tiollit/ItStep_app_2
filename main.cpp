5ff0687d67b99d30fd5157d9fd01e3c631f3b8a0
Tiollit <Tiollit7@gmail.com>
2022-10-10 18:03:41 +0200

    Lab 10 begin

--- main.cpp
+++ main.cpp
@@ -1,44 +1,10 @@
-#include <iostream>
+#include <iostream> //Користувач вводить ціле число А. Програма має визначити, що куб суми цифр цього числа дорівнює А* А.
 using namespace std;
 int main()
 {
-    //----------------Пред-перевірки--------------
-    /*int size;
-    cout << "=>";
-    cin >> size;
-    int i = size;*/
-    /*while(i < 10)
-    {
-        cout << "* ";
-        i++;
-    }*/
-    //----------------Пост-перевірки---------------
-    //int age;
-    ////
-    //do
-    //{
-    //  system("cls");
-    //  cout << "Enter your age: ";
-    //  cin >> age;
-    //  /*if (age >= 18)
-    //  {
-    //      cout << "Sucsses!" << endl;
-    //      system("pause");
-    //      break;
-    //  }
-    //  else
-    //  {
-    //      cout << "Error! Incorect age: Your age must be more 18+" << endl;
-    //      system("pause");
-    //  }*/
-    //  
-    //} while (age < 18);
-    //system("cls");
-    //cout << "Exit..." << endl;
-    //----------------Пошаговий---------------
-    for (int i = 0; i < 10; i += 3)
-    {
-        cout << i << "\t";
-    }
-    cout << endl;
+    int a;
+    cout << "Incert number: " << endl;
+    cin >> a;
+
     return 0;
+}
