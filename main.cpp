5fe048a9dc9a6c85fef7a889b7e232af6b1d37bf
Tiollit <Tiollit7@gmail.com>
2022-10-10 17:01:23 +0200

    Lab 8 done

--- main.cpp
+++ main.cpp
@@ -2,23 +2,28 @@
 using namespace std;
 int main()
 {
-    int a, b, d, e, c = 1, n = 1;
+    int a, b, d = 0, e, c = 1, n = 1, p;
     cout << "Incert random number:" << endl;
     cin >> a;
-    e = a; d = a;
-    while ((e /= 10) > 0) n++;                 // n - кількість цифр в числі
-    // cout << n;    
-    for (int i = n; i >= 0; i--)
-    {
-        for (int k = n; k >= 0; k--)
+    e = a;
+    while ((e /= 10) > 0) n++;                        // n - кількість цифр в числі
+    // cout << n; 
+    p = n - 1;
+    for (int i = n; i > 0; i--)
+    {        
+        for (int k = p; k >= 0; k--)
         {
             c = c * 10;                            
-        }
-        b = a / c;                             // перша цифра числа
-        if (b == 3 || b == 6) d = d - b * c;   // прибираємо цифру якщо вона = 3 або 6 але залишаємо 0
-        a = a - b * c;                         // забираємо першу цифру
+        }                                   
+        b = a / c;                                    // перша цифра числа і перевірка останньої
+        if (b == 3 || b == 6) d = d / 10;             // прибираємо цифру якщо вона = 3 або 6 і скорочуємо на 1 знак вправо           
+        else d = d + b * c;                                         
+        a = a - b * c;                                // забираємо першу цифру   
+        p = p - 1;
         c = 1;
-        n = n - 1;
+        if (a < 10 && a != 6 && a != 3) d = d + a;    
+        else if (a < 10 && a == 3) d = d / 10;
+        else if (a < 10 && a == 6) d = d / 10;
     }
     cout << "New number is: " << d << endl;
     return 0;
