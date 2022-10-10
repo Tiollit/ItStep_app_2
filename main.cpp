fc86a5938ab43db3c48ee13f113a5c299bd9dc07
Tiollit <Tiollit7@gmail.com>
2022-10-10 18:43:05 +0200

    Lab 12 done

--- main.cpp
+++ main.cpp
@@ -1,44 +1,20 @@
-#include <iostream>
+#include <iostream> // Користувач вводить два цілих числа. Необхідно вивести всі цілі числа, на які обидва введені числа діляться без залишку
 using namespace std;
 int main()
 {
-    //----------------Пред-перевірки--------------
-    /*int size;
-    cout << "=>";
-    cin >> size;
-    int i = size;*/
-    /*while(i < 10)
+    int c, d;
+    double a, b, e, f, g, h;
+    cout << "Incert 2 random numbers: " << endl;
+    cin >> a >> b;
+    for (int i = 1; i <= a; i++)                     // Немає хначення а чи б більше
     {
-        cout << "* ";
-        i++;
-    }*/
-    //----------------Пост-перевірки---------------
-    //int age;
-    ////
-    //do
-    //{
-    //  system("cls");
-    //  cout << "Enter your age: ";
-    //  cin >> age;
-    //  /*if (age >= 18)
-    //  {
-    //      cout << "Sucsses!" << endl;
-    //      system("pause");
-    //      break;
-    //  }
-    //  else
-    //  {
-    //      cout << "Error! Incorect age: Your age must be more 18+" << endl;
-    //      system("pause");
-    //  }*/
-    //  
-    //} while (age < 18);
-    //system("cls");
-    //cout << "Exit..." << endl;
-    //----------------Пошаговий---------------
-    for (int i = 0; i < 10; i += 3)
-    {
-        cout << i << "\t";
+        c = a / i;
+        d = b / i;
+        e = a / i;
+        f = b / i;
+        g = e - c;
+        h = f - d;
+        if (g == 0 && h == 0) cout << i << " ";
     }
-    cout << endl;
     return 0;
+}
